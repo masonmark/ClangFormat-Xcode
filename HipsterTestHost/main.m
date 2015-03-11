a698f34d8f9a27ac7d85baa9020a82219b33a022
Mason Mark <mason@fivespeed.com>
2015-03-11 11:50:00 +0900

    Add a test host app for automated tests.
    Also, remove a reference to the project file itself that had (accidentally?) been added to the 'Supporting Files' group.
    
    Also, add the additional IDE turds that Xcode left.

---
+++ HipsterTestHost/main.m
@@ -0,0 +1,7 @@
+// main.m HipsterTestHost Created by mason on 2015-03-11. This file is in the public domain.
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
---
+++ HipsterTests/HipsterTests.m
@@ -0,0 +1,18 @@
+// HipsterTestHostTests.m Created by mason on 2015-03-11. This file is in the public domain.
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+
+@interface HipsterTests : XCTestCase
+@end
+
+
+@implementation HipsterTests
+
+- (void)testExample
+{
+    XCTFail(@"Tests are not yet implemented.");
+}
+
+@end
